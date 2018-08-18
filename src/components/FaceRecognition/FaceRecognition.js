9623d729ab8685b462ef9dd13f68ad71902dc491
Ben Collins <ben@Bens-MacBook-Air.local>
2018-08-17 19:01:58 -0500

    adding front-end application

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './FaceRecognition.css'
+
+const FaceRecognition = ({ imageUrl, boxes }) => {
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img id='inputimage' src={imageUrl} alt='' width='500px' height='auto'></img>
+        {boxes.map((box, index) => {
+          return <div key={index} className='bounding-box' style={{
+            top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol
+          }}></div>
+        })}
+
+      </div>
+    </div>
+  )
+}
+
+export default FaceRecognition;
